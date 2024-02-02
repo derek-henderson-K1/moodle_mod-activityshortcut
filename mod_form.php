2f72a31bc63289503d63131b2922c49fc575414e
Derek <derek@pop-os.localdomain>
2024-02-02 08:49:33 -0500

    initial commit

---
+++ mod_form.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The main mod_activityshortcut configuration form.
+ *
+ * @package     mod_activityshortcut
+ * @copyright   2024 derek.henderson <derek.henderson@knowledgeone.ca>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot.'/course/moodleform_mod.php');
+require_once($CFG->dirroot.'/mod/activityshortcut/lib.php');
+
+/**
+ * Module instance settings form.
+ *
+ * @package    mod_activityshortcut
+ * @copyright  2024 derek.henderson <derek.henderson@knowledgeone.ca>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_activityshortcut_mod_form extends moodleform_mod {
+
+    /**
+     * Defines forms elements
+     */
+    public function definition() {
+        global $CFG;
+        $plugin = 'mod_activityshortcut';
+        $mform = $this->_form;
+        $mform->addElement('header', 'general', get_string('general', 'form'));
+        // Adding the standard "name" field.
+        $mform->addElement('text', 'name', get_string('activityshortcutname', 'mod_activityshortcut'), array('size' => '64'));
+        if (!empty($CFG->formatstringstriptags)) {
+            $mform->setType('name', PARAM_TEXT);
+        } else {
+            $mform->setType('name', PARAM_CLEANHTML);
+        }
+
+        $mform->addRule('name', null, 'required', null, 'client');
+        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addHelpButton('name', 'activityshortcutname', 'mod_activityshortcut');
+
+        // Adding the standard "intro" and "introformat" fields.
+        if ($CFG->branch >= 29) {
+            $this->standard_intro_elements();
+        } else {
+            $this->add_intro_editor();
+        }
+
+        $activitylist = activityshortcut_get_activities_list();
+        
+        $mform->addElement('select', 'relatedactivity', get_string('relatedactivity', $plugin),$activitylist);
+                 
+        $mform->addElement('hidden', 'completionunlocked', 0);
+            $mform->setType('completionunlocked', PARAM_INT);
+
+ $this->standard_hidden_coursemodule_elements();
+
+       /// $this->plugin_extend_coursemodule_standard_elements();
+        // Add standard buttons.
+        $this->add_action_buttons();
+    }
+}
